48cba56b6d315947deb19194d82813a6c095e2d4
Yuliia Kushnir <lavrinenkoyuli@gmail.com>
2026-05-21 09:48:24 +0300

    Changed properties

--- src/main/java/org/example/orderservice/OrderServiceApplication.java
+++ src/main/java/org/example/orderservice/OrderServiceApplication.java
@@ -1,6 +1,6 @@
 package org.example.orderservice;
 
-import io.github.cdimascio.dotenv.Dotenv;
+//import io.github.cdimascio.dotenv.Dotenv;
 import org.example.orderservice.config.PricingProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,16 +11,17 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 public class OrderServiceApplication {
     public static void main(String[] args) {
 
-        Dotenv dotenv = Dotenv.load();
-        System.setProperty("RABBITMQ_USER", dotenv.get("RABBITMQ_USER"));
-        System.setProperty("RABBITMQ_PASSWORD", dotenv.get("RABBITMQ_PASSWORD"));
-
-        System.setProperty("POSTGRES_USER", dotenv.get("POSTGRES_USER"));
-        System.setProperty("POSTGRES_PASSWORD", dotenv.get("POSTGRES_PASSWORD"));
-
-        System.setProperty("CLOUDINARY_CLOUD_NAME", dotenv.get("CLOUDINARY_CLOUD_NAME"));
-        System.setProperty("CLOUDINARY_API_KEY", dotenv.get("CLOUDINARY_API_KEY"));
-        System.setProperty("CLOUDINARY_API_SECRET", dotenv.get("CLOUDINARY_API_SECRET"));
+//        Dotenv dotenv = Dotenv.load();
+//        System.setProperty("RABBITMQ_USER", dotenv.get("RABBITMQ_USER"));
+//        System.setProperty("RABBITMQ_PASSWORD", dotenv.get("RABBITMQ_PASSWORD"));
+//
+//        System.setProperty("POSTGRES_USER", dotenv.get("POSTGRES_USER"));
+//        System.setProperty("POSTGRES_PASSWORD", dotenv.get("POSTGRES_PASSWORD"));
+//
+//        System.setProperty("CLOUDINARY_CLOUD_NAME", dotenv.get("CLOUDINARY_CLOUD_NAME"));
+//        System.setProperty("CLOUDINARY_API_KEY", dotenv.get("CLOUDINARY_API_KEY"));
+//        System.setProperty("CLOUDINARY_API_SECRET", dotenv.get("CLOUDINARY_API_SECRET"));
+//
         SpringApplication.run(OrderServiceApplication.class, args);
     }
 
